8dc4595d3fafc92dd368fbbfba09ae01ed7796fe
Cosimo Ranieri <co.ranieri@gmail.com>
2013-08-22 16:53:54 +0200

    unnecessary method removed

--- lib/prime_table.rb
+++ lib/prime_table.rb
@@ -18,20 +18,4 @@ class PrimeTable
       print "\n"
     end
   end
-
-  def print_mult_table_2
-    to_console = ''
-    to_console += '     '
-    @prime_numbers.each {|i| to_console += "%-3d  " % i}
-    to_console += "\n     "
-    @prime_numbers.each {|i| to_console += '---- '}
-    to_console += "\n"
-
-    @prime_numbers.each do |j|
-      to_console += "%-3d| " % j
-      @prime_numbers.each {|i| to_console += "%-3d  " % (i*j)}
-      to_console += "\n"
-    end
-    to_console
-  end
 end
